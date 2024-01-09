6a687594474d0e25f7c681fea0e8dd383fc0228f
tutelugu <142389771+tutelugu@users.noreply.github.com>
2024-01-09 19:22:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,10 +48,10 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Tulinks.online')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'c334aad5561c4dadebbab70eaade19cab12d7b93')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
